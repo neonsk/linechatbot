222eafdc976bee60fe97c6f9d080e11516ad0db5
neonsk <shota.k.neon@gmail.com>
2019-02-06 15:05:01 +0900

    天気

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from linebot import LineBotApi,WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent,TextMessage,TextSendMessage
 import os
+import requests
+import pprint
 
 app=Flask(__name__)
 #環境変数の取得
@@ -26,7 +28,22 @@ def callback():
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
-    reply_text = event.message.text
+    #入力された文字列を格納
+    push_text = event.message.text
+
+    #リプライする文字列
+    if push_text == "天気":
+        url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
+        api_data = requests.get(url).json()
+        for weather in api_data['forecasts']:
+            weather_date = weather['dateLabel']
+            weather_forecasts = weather['telop']
+            print(weather_date + ':' + weather_forecasts)
+        reply_text = api_data["description"]["text"]
+    else:
+        reply_text = push_text
+
+    #リプライ部分の記述
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text+"。でもなおに会いたいな"))
 
 if __name__=="__main__":
