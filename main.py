b5f550f93e7b1d728237638bc7043b5db1b3bb1a
neonsk <shota.k.neon@gmail.com>
2019-02-10 21:19:51 +0900

    talkapi

--- main.py
+++ main.py
@@ -46,13 +46,16 @@ def handle_message(event):
         reply_text = push_text
     """
     #A3RTのTalkAPIにより応答
-    apikey = "DZZTN8HTduABGoAd8GPaM3QCvapddGU7"
-    client = pya3rt.TalkClient(apikey)
-    response = client.talk(push_text)
-    reply_text = ((response['results'])[0])['reply']
+    reply_text = talkapi_response(push_text)
 
     #リプライ部分の記述
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text))
+
+def talkapi_response(text):
+    apikey = "DZZTN8HTduABGoAd8GPaM3QCvapddGU7"
+    client = pya3rt.TalkClient(apikey)
+    response = client.talk(text)
+    return ((response['results'])[0])['reply']
 
 if __name__=="__main__":
     port=int(os.getenv("PORT",5000))
